2dd083948b8d8701f9960bfa0e056c4302483196
ceidnerdpatras <georgepapa123gt@gmail.com>
2026-05-26 14:59:53 +0300

    Refactor FarmSyncUseCase9 to Main class

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-public class FarmSyncUseCase9 {
+public class Main {
     public static void main(String[] args) {
         System.out.println("=========================================================================");
         System.out.println("FARMSYNC SYSTEM - RUNNING USE CASE 9");
